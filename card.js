b5c0b1ceefa60b6942c8a333285f20af3a261c29
Boburtoirjonov <toirjonovbobur1@gmail.com>
2025-08-07 06:08:43 +0500

    tailwind updated

---
+++ card.js
@@ -0,0 +1,45 @@
+import StarIcon from '@mui/icons-material/Star';
+export const data = [
+    {
+        id: 1,
+        img: 'https://free-png.ru/wp-content/uploads/2024/09/free-png.ru-j83snaffi3s0k43e4t8rf64xc7d38k4l.png',    
+        name: 'Premium Wireless Headphones',
+        rating: <StarIcon/>,
+        price: '299'
+    },
+    {
+        id: 2,
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9Yq627QG8KObR9AGk0B-ukJco2PH77TLi3Q&s',    
+        name: 'Luxury Minimalist Watch',
+        rating: <StarIcon/>,
+        price: '549'
+    },
+    {
+        id: 3,
+        img: '',    
+        name: 'Premium Wireless Headphones',
+        rating: <StarIcon/>,
+        price: '299'
+    },
+    {
+        id: 4,
+        img: 'https://free-png.ru/wp-content/uploads/2024/09/free-png.ru-j83snaffi3s0k43e4t8rf64xc7d38k4l.png',    
+        name: 'Premium Wireless Headphones',
+        rating: <StarIcon/>,
+        price: '299'
+    },
+    {
+        id: 5,
+        img: 'https://free-png.ru/wp-content/uploads/2024/09/free-png.ru-j83snaffi3s0k43e4t8rf64xc7d38k4l.png',    
+        name: 'Premium Wireless Headphones',
+        rating: <StarIcon/>,
+        price: '299'
+    },
+    {
+        id: 6,
+        img: 'https://free-png.ru/wp-content/uploads/2024/09/free-png.ru-j83snaffi3s0k43e4t8rf64xc7d38k4l.png',    
+        name: 'Premium Wireless Headphones',
+        rating: <StarIcon/>,
+        price: '299'
+    },
+]
---
+++ src/Components/Products.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Products = () => {
+  return (
+    <div className='container'>
+        <h2>Featured Products</h2>
+        <p>Carefully curated products that blend innovation with timeless design</p>
+        <div className="cardWrapper">
+
+        </div>
+    </div>
+  )
+}
